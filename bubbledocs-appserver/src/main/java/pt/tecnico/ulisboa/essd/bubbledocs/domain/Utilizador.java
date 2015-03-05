80ef0b79f43075718ff86a0501971d8a2b49666d
Pedro Dias <pedro@pedro.dias>
2015-03-05 15:49:35 +0000

    Alteracoes no Utilizador.

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
@@ -1,9 +1,29 @@
 package pt.tecnico.ulisboa.essd.bubbledocs.domain;
 
+import pt.ist.fenixframework.FenixFramework;
+
 public class Utilizador extends Utilizador_Base {
     
     public Utilizador() {
         super();
     }
+    
+    
+    
+    public void removeFolha(String nome){
+    	FolhadeCalculo toRemove = getNomeFolha(nome);
+    	
+    	if(toRemove ==  null){
+    		System.out.println("Folha inexistente");
+    	}
+    	
+    	removeFolha(toRemove);
+    	
+    	
+    	
+    	
+    }
+    
+   
     
 }
