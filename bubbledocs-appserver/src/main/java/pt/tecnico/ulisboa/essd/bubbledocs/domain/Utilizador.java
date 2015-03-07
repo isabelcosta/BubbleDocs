5c0a76a1ddf9855452f7ae6f435d71f791d8c870
Pedro Dias <pedro@pedro.dias>
2015-03-07 20:11:28 +0000

    Utilizador.java ALterações

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
@@ -4,26 +4,66 @@ import pt.ist.fenixframework.FenixFramework;
 
 public class Utilizador extends Utilizador_Base {
     
-    public Utilizador() {
+    public Utilizador(String nomeUtilizador, String userName, String password) {
         super();
+        setNome(nomeUtilizador);
+		setUsername(userName);
+		setPassword(password);
+        
     }
     
     
+    //Cria uma folha
+    
+    public void criaFolha( String criador, String nomeFolha, int linha , int coluna){
+    	
+    		FolhadeCalculo folha = new FolhadeCalculo(criador, nomeFolha, linha, coluna);
+    		
+    		this.addFolhascriadas(folha);
+    }
+    
+    //Remove uma folha 
     
     public void removeFolha(String nome){
-    	FolhadeCalculo toRemove = getNomeFolha(nome);
     	
-    	if(toRemove ==  null){
-    		System.out.println("Folha inexistente");
-    	}
-    	
-    	removeFolha(toRemove);
-    	
-    	
-    	
-    	
+    	for(FolhadeCalculo folha : this.getFolhascriadasSet()){
+    		if(folha.getNomeFolha().equals(nome)){
+    			folha.
+    			
+    			//Terminar
+    			
+    			
+    		}	
+    	}	
     }
     
-   
+    //Inserir numa folha
     
+    public void insereCelula(String nomefolha, int linha, int coluna, String conteudo, String nomeUtilizador){
+    	
+    	for(FolhadeCalculo folha : this.getFolhascriadasSet()){
+    		if(folha.getNomeFolha().equals(nomefolha)){
+    			folha.criaCelula(linha, coluna, conteudo);
+   	
+    		}
+    	}	
+    }
+    
+    //Adicionar utilizador - operação executada pela root
+    
+    public void adicionaUtilizador(String nomeUtilizador, String userName, String password){
+    	
+    		new Utilizador(nomeUtilizador, userName, password);
+    	}
+    
+    
+    //Remove utilizador - operação executada pela root
+    
+	public void removeUtilizador(String username){
+		
+		
+		
+    		
+    	
+    }    
 }
