9fbf8c72f2a6a0cc295b5bf25f7fe947da61445a
Pedro Dias <pedro@pedro.dias>
2015-03-08 16:45:38 +0000

    BubbleAPlication.java v1

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/BubbleApplication.java
@@ -0,0 +1,45 @@
+/*
+package pt.tecnico.ulisboa.essd.bubbledocs;
+
+import java.util.Set;
+
+import javax.transaction.SystemException;
+
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
+import pt.ist.fenixframework.TransactionManager;
+import pt.tecnico.ulisboa.essd.bubbledocs.domain.*;
+
+
+public class BubbleApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Bubble application");
+
+		TransactionManager tm = FenixFramework.getTransactionManager();
+		boolean committed = false;
+
+
+		try {
+			tm.begin();
+
+			Utilizador utilizador = Utilizador
+
+			for(Utilizador user : ){}
+
+
+			tm.commit();
+			committed = true;
+		}catchSystemException| NotSupportedException | RollbackException| HeuristicMixedException | HeuristicRollbackException ex) {
+			System.err.println("Error in execution of transaction: " + ex);
+		} finally {
+			if (!committed) 
+				try {
+					tm.rollback();
+				} catch (SystemException ex) {
+					System.err.println("Error in roll back of transaction: " + ex);
+				}
+			}
+		}
+	} */
