7081c7470bc9d7f53e2ec8586521f10d9d82e424
Pedro Dias <pedro@pedro.dias>
2015-03-10 19:37:01 +0000

    Actualização do User

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
@@ -1,5 +1,7 @@
 package pt.tecnico.ulisboa.essd.bubbledocs.domain;
 
+import pt.ist.fenixframework.FenixFramework;
+
 public class Utilizador extends Utilizador_Base {
     
     public Utilizador(String nomeUtilizador, String userName, String password) {
@@ -9,4 +11,34 @@ public class Utilizador extends Utilizador_Base {
 		setPassword(password);
     }
     
+    public void criaFolha(String nome, int linhas, int colunas, int id){
+    	
+    	
+    	FolhadeCalculo folha = new FolhadeCalculo();
+    	
+    	folha.setDono(nome);
+    	folha.setLinhas(linhas);
+    	folha.setColunas(colunas);
+    	folha.setID(id);
+    	
+    	this.addFolhascriadas(folha);
+    	
+    }
+    
+    public void removeFolha(String nome){
+    	
+    	for(FolhadeCalculo folha : getFolhascriadasSet()){
+    		
+    		if(folha.getNomeFolha().equals(nome)){
+    			folha.delete();
+    			this.removeFolhascriadas(folha);
+    		}
+    	}
+    }
+    
+    public void apagaFolhas(){
+    	
+    	//PErcorre a folha e apaga com o remove folhas
+    	//No 
+    }
 }
