ca7c50ecb0ee952f851d3f56602a81ed2e1df17b
Pedro Dias <pedro@pedro.dias>
2015-03-11 00:43:46 +0000

    Actualização do User

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
@@ -6,42 +6,43 @@ public class Utilizador extends Utilizador_Base {
     public Utilizador(String nomeUtilizador, String userName, String password) {
         super();
         setNome(nomeUtilizador);
-		setUsername(userName);
-		setPassword(password);
+        setUsername(userName);
+        setPassword(password);
     }
     
-    public void criaFolha(String nome, int linhas, int colunas, int id){
-    	
-    	FolhadeCalculo folha = new FolhadeCalculo();
-    	
-    	folha.setDono(nome);
-    	folha.setLinhas(linhas);
-    	folha.setColunas(colunas);
-    	folha.setID(id);
-    	
-    	this.addFolhascriadas(folha);
-    	
+    public void criaFolha(String nomeFolha,String nome, int linhas, int colunas, int id){
+        
+        FolhadeCalculo folha = new FolhadeCalculo();
+        
+        folha.setNomeFolha(nomeFolha);
+        folha.setDono(nome);
+        folha.setLinhas(linhas);
+        folha.setColunas(colunas);
+        folha.setID(id);
+        
+        this.addFolhascriadas(folha);
+        
     }
     
     
     public void removeFolha(String nome){
-    	
-    	for(FolhadeCalculo folha : getFolhascriadasSet()){
-    		if(folha.getNomeFolha().equals(nome)){
-    			folha.delete();
-    			this.removeFolhascriadas(folha);
-    		}
-    	}
+        
+        for(FolhadeCalculo folha : getFolhascriadasSet()){
+            if(folha.getNomeFolha().equals(nome)){
+                folha.delete();
+                this.removeFolhascriadas(folha);
+            }
+        }
     }
     
    
     public void apagaFolhas(){
-    	
-    	String nome;
-    	
-    	for(FolhadeCalculo folha : getFolhascriadasSet()){
-    		nome = folha.getNomeFolha();
-    		this.removeFolha(nome);	
-    	}	
+        
+        String nome;
+        
+        for(FolhadeCalculo folha : getFolhascriadasSet()){
+            nome = folha.getNomeFolha();
+            this.removeFolha(nome); 
+        }   
     }
 }
