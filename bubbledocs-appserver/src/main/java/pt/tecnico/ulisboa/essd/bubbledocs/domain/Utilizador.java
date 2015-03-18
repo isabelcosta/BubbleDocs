f51809080a39d671ebe98eca0cdebae0258e1dc9
Fabio Carvalho <frc.foxman@gmail.com>
2015-03-18 12:55:58 +0000

    Closes #16

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
@@ -1,5 +1,8 @@
 package pt.tecnico.ulisboa.essd.bubbledocs.domain;
 
+import pt.ist.fenixframework.FenixFramework;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.DontHavePermissionException;
+
 
 public class Utilizador extends Utilizador_Base {
     
@@ -45,4 +48,95 @@ public class Utilizador extends Utilizador_Base {
             this.removeFolha(nome); 
         }   
     }
+    
+    
+    
+    /* 
+     * 
+     * Atribuir e retirar permissoes nas suas folhas 
+     * 
+     * NOTA: permissao so recebe "leitura" ou "escrita"
+     * 
+     * */	
+    public void darPermissoes(String permissao, String nomeUtilizadorAdicionar, String nomeFolha) {
+    	
+   
+    	Utilizador utilizador=null;
+    	for(Utilizador u : FenixFramework.getDomainRoot().getUtilizadoresSet()){
+    		if (u.getNome().equals(nomeUtilizadorAdicionar))
+    			utilizador=u;
+    	}
+    	
+    	for(FolhadeCalculo folha : this.getFolhascriadasSet()){
+    		if (folha.getNomeFolha().equals(nomeFolha)){
+    			if (this.getNome().equals(folha.getDono())){
+    				switch(permissao){
+    				case("escrita"):
+    					folha.addUtilizadores_e(utilizador);
+    				case("leitua"):
+    					folha.addUtilizadores_l(utilizador);
+    				}
+    			}
+    			
+    			else{
+        			try{
+        				folha.podeEscrever(this.getNome());
+        				switch(permissao){
+        				case("escrita"):
+        					folha.addUtilizadores_e(utilizador);
+        				case("leitua"):
+        					folha.addUtilizadores_l(utilizador);
+        				}
+        			}catch(DontHavePermissionException dhpe){
+        				System.out.println(this.getNome()+" não tem permissões para alterar a folha");	
+        			}
+    			}
+    		}
+    	}
+    
+ 	}
+    
+    
+    public void retirarPermissoes(String permissao, String nomeUtilizadorAdicionar, String nomeFolha) {
+    	
+    	   
+    	Utilizador utilizador=null;
+    	for(Utilizador u : FenixFramework.getDomainRoot().getUtilizadoresSet()){
+    		if (u.getNome().equals(nomeUtilizadorAdicionar))
+    			utilizador=u;
+    	}
+    	
+    	for(FolhadeCalculo folha : this.getFolhascriadasSet()){
+    		if (folha.getNomeFolha().equals(nomeFolha)){
+    			if (this.getNome().equals(folha.getDono())){
+    				switch(permissao){
+    				case("escrita"):
+    					folha.removeUtilizadores_e(utilizador);
+    				case("leitua"):
+    					folha.removeUtilizadores_l(utilizador);
+    				}
+    			}
+    			
+    			else{
+        			try{
+        				folha.podeEscrever(this.getNome());
+        				switch(permissao){
+        				case("escrita"):
+        					folha.removeUtilizadores_e(utilizador);
+        				case("leitua"):
+        					folha.removeUtilizadores_l(utilizador);
+        				}
+        			}catch(DontHavePermissionException dhpe){
+        				System.out.println(this.getNome()+" não tem permissões para alterar a folha");	
+        			}
+    			}
+    		}
+    	}
+    }
+
+
 }
+    		
+    		
+    	
+    
