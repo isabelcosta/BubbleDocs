efdfe1142afd07df8fe4d5ba6dcbb135dae2b160
Fábio Carvalho <frc.foxman@gmail.com>
2015-03-19 20:38:07 +0000

    Delete BubbleDocsException.java
    Afinal tive de apagar aqui a pasta Java ficheiro a ficheiro

--- bubbledocs-appserver/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/BubbleDocsException.java
+++
@@ -1,7 +0,0 @@
-package pt.tecnico.ulisboa.essd.bubbledocs.exception;
-
-public abstract class BubbleDocsException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-}
