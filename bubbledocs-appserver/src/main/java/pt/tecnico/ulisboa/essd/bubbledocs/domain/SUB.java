8cf07196bf590df04299db9ff2efb060b32f16f9
Pedro Dias <psdd91@gmail.com>
2015-03-20 11:05:03 +0000

    Sub sem delete()

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/SUB.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/SUB.java
@@ -25,12 +25,4 @@ public class SUB extends SUB_Base {
     	
 		return element;
 	}
-    
-    public void delete() {
-    	setCelula(null);
-    	setArgumento1(null);
-    	setArgumento2(null);
-    	deleteDomainObject();
-    	
-    }
 }
