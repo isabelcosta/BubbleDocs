a048d983ef516ebc879635a4648eb631c7fa3e66
Fabio Carvalho <frc.foxman@gmail.com>
2015-03-22 15:19:46 +0000

    Teste do CreateSpreadSheet

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/tests/CreateSpreadSheetTest.java
@@ -0,0 +1,73 @@
+package pt.tecnico.ulisboa.essd.bubbledocs.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import pt.tecnico.ulisboa.essd.bubbledocs.domain.Bubbledocs;
+import pt.tecnico.ulisboa.essd.bubbledocs.domain.FolhadeCalculo;
+import pt.tecnico.ulisboa.essd.bubbledocs.domain.Utilizador;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.ArgColunaInvalidoException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.ArgLinhaInvalidoException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.UtilizadorInvalidoException;
+import pt.tecnico.ulisboa.essd.bubbledocs.services.CreateSpreadSheet;
+
+
+
+public class CreateSpreadSheetTest extends BubbleDocsServiceTest {
+
+	// the tokens
+  	 private String frc;
+
+	private static final String USERNAME = "frc";
+	private static final String PASSWORD = "frc";
+	private static final String SPREADSHEET_NAME = "myFolha";
+	private static final int ROWS = 100;
+	private static final int BAD_ROW = -1;
+	private static final int COLUMNS = 100;
+	private static final int BAD_COLUMN = -1;
+	private static final String BAD_USER_TOKEN = "false-token";
+  
+  @Override
+  public void populate4Test() {
+	  	//createUser(USERNAME, PASSWORD, "Fabio Carvalho");
+	  	//frc = addUserToSession("frc");
+  	
+  }
+
+  @Test
+  public void success() {
+  	
+      CreateSpreadSheet service = new CreateSpreadSheet( frc, SPREADSHEET_NAME, ROWS, COLUMNS);
+      service.execute();
+      
+      String sheetName=null;
+      for (FolhadeCalculo f : Bubbledocs.getInstance().getFolhasSet()){
+    	  if (f.getDono().equals("frc"))
+    		   sheetName = f.getNomeFolha();
+      }
+      
+      assertEquals(SPREADSHEET_NAME,sheetName);
+  }
+  
+  @Test (expected = UtilizadorInvalidoException.class)
+  public void invalidToken(){
+	  CreateSpreadSheet service = new CreateSpreadSheet( BAD_USER_TOKEN, SPREADSHEET_NAME, ROWS, COLUMNS);
+      service.execute();
+  } 
+  
+  @Test (expected = ArgColunaInvalidoException.class)
+  public void invalidColumn(){
+	  CreateSpreadSheet service = new CreateSpreadSheet( BAD_USER_TOKEN, SPREADSHEET_NAME, ROWS, BAD_COLUMN);
+      service.execute();
+  }
+  
+  @Test (expected = ArgLinhaInvalidoException.class)
+  public void invalidRow(){
+	  CreateSpreadSheet service = new CreateSpreadSheet( BAD_USER_TOKEN, SPREADSHEET_NAME, BAD_ROW, COLUMNS);
+      service.execute();
+  }
+  
+  
+  
+}
