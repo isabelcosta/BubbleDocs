7fbb8fe90019e3c4b91f7c0f6f4a721f5a595d19
Fabio Carvalho <frc.foxman@gmail.com>
2015-04-11 16:43:43 +0100

    Closes #49 - Alterar agora tudo o que cria um novo user

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/domain/Utilizador.java
@@ -1,20 +1,35 @@
 package pt.tecnico.ulisboa.essd.bubbledocs.domain;
 
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.UtilizadorInvalidoException;
+
 
 public class Utilizador extends Utilizador_Base {
     
-    public Utilizador(String nomeUtilizador, String userName, String password) {
+    public Utilizador(String nomeUtilizador, String userName, String email) {
         super();
-        setNome(nomeUtilizador);
-        setUsername(userName);
-        setPassword(password);
+        
+        try {
+        	if (nameIsValid(userName)){
+        		setNome(nomeUtilizador);
+                setUsername(userName);
+                setEmail(email);
+        	}	
+        }catch (UtilizadorInvalidoException ex) {
+			System.err.println("O username tem de ter entre 3 e 8 caracteres");
+        	
+        }
     }
     
 
-    
-
-    
-    
+    private boolean nameIsValid(String username) throws UtilizadorInvalidoException{
+    	int size = username.length();
+    	
+    	if (size >=3 && size <=8)
+    		return true;
+    	else
+    		throw new UtilizadorInvalidoException();
+    }
+    	
 }
     		
     		
