f9bae3fea11256c881baaedeafecc0c228a6175a
Ines Garcia <inescunhagarcia@hotmail.com>
2015-04-15 00:03:40 +0100

    Closes #54

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/CannotLoadDocumentException.java
@@ -0,0 +1,8 @@
+package pt.tecnico.ulisboa.essd.bubbledocs.exception;
+
+public class CannotLoadDocumentException extends BubbleDocsException {
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/services/remote/StoreRemoteServices.java
@@ -0,0 +1,19 @@
+package pt.tecnico.ulisboa.essd.bubbledocs.services.remote;
+
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.CannotStoreDocumentException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.CannotLoadDocumentException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.RemoteInvocationException;
+
+
+public class StoreRemoteServices {
+
+	public void storeDocument(String username, String docName, byte[] document)
+			throws CannotStoreDocumentException, RemoteInvocationException {
+		// TODO : the connection and invocation of the remote service
+	}
+
+//	public byte[] loadDocument(String username, String docName)
+//			throws CannotLoadDocumentException, RemoteInvocationException {
+//		// TODO : the connection and invocation of the remote service
+//	}
+}
