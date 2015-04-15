05a9fdd6ded9b4b13a256421940bc0eba1c80b38
vicente <vicente@vicente>
2015-04-15 10:24:12 +0100

    criação das excepções

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/LoginBubbleDocsException.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/LoginBubbleDocsException.java
@@ -1,7 +1,26 @@
 package pt.tecnico.ulisboa.essd.bubbledocs.exception;
 
+<<<<<<< Upstream, based on origin/master
 public class LoginBubbleDocsException extends BubbleDocsException {
 
 	private static final long serialVersionUID = 1L;
 	
 }
+=======
+public class LoginBubbleDocsException extends BubbleDocsException{
+	private static final long serialVersionUID = 1L;
+
+	private String loginExp;
+	
+	public LoginBubbleDocsException() {
+    }
+	
+	public LoginBubbleDocsException(String loginExp) {
+		this.loginExp = loginExp;
+	}
+	
+	public String getconflictingSpreadSheet() {
+		return this.loginExp;
+	}
+}
+>>>>>>> 843b669 criação das excepções
--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/RemoteInvocationException.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/RemoteInvocationException.java
@@ -1,7 +1,20 @@
 package pt.tecnico.ulisboa.essd.bubbledocs.exception;
 
-public class RemoteInvocationException extends BubbleDocsException {
-
+public class RemoteInvocationException extends BubbleDocsException{
 	private static final long serialVersionUID = 1L;
+
+	private String remInvExp;
+	
+	public RemoteInvocationException() {
+    }
+	
+	public RemoteInvocationException(String remInvExp) {
+		this.remInvExp= remInvExp;
+	}
+	
+	public String getconflictingSpreadSheet() {
+		return this.remInvExp;
+	}
+}
 	
 }
