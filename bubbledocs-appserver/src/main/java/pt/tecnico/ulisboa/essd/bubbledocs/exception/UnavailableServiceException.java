d070e524b92588a37a914d8db80e988f3cc8f558
Ines Garcia <inescunhagarcia@hotmail.com>
2015-04-15 12:15:11 +0100

    Excepcao UnavailableServiceException

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/UnavailableServiceException.java
@@ -0,0 +1,7 @@
+package pt.tecnico.ulisboa.essd.bubbledocs.exception;
+
+public class UnavailableServiceException extends BubbleDocsException {
+
+	private static final long serialVersionUID = 1L;
+	
+}
