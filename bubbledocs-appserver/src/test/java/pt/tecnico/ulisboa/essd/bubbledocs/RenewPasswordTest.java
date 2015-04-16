7e393263696aa6688f310d9b6a8f88038aad6b40
Ines Garcia <inescunhagarcia@hotmail.com>
2015-04-16 14:56:01 +0100

    new

--- bubbledocs-appserver/src/test/java/pt/tecnico/ulisboa/essd/bubbledocs/RenewPasswordTest.java
+++ bubbledocs-appserver/src/test/java/pt/tecnico/ulisboa/essd/bubbledocs/RenewPasswordTest.java
@@ -1,86 +1,107 @@
 package pt.tecnico.ulisboa.essd.bubbledocs;
 
-import java.sql.ResultSet;
-
+import static org.junit.Assert.assertEquals;
 import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
 import mockit.Mocked;
+import mockit.StrictExpectations;
 
+import org.joda.time.LocalDate;
 import org.junit.Test;
 
+import pt.tecnico.ulisboa.essd.bubbledocs.domain.Bubbledocs;
+import pt.tecnico.ulisboa.essd.bubbledocs.domain.FolhadeCalculo;
+import pt.tecnico.ulisboa.essd.bubbledocs.domain.Token;
+import pt.tecnico.ulisboa.essd.bubbledocs.domain.Utilizador;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.LoginBubbleDocsException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.ProtectedCellException;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.RemoteInvocationException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.UnauthorizedOperationException;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.UnavailableServiceException;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.UserNotInSessionException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.NotLiteralException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.OutOfBoundsException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.SpreadSheetDoesNotExistException;
+import pt.tecnico.ulisboa.essd.bubbledocs.services.DeleteUser;
 import pt.tecnico.ulisboa.essd.bubbledocs.services.RenewPasswordService;
+import pt.tecnico.ulisboa.essd.bubbledocs.services.CreateSpreadSheet;
 import pt.tecnico.ulisboa.essd.bubbledocs.services.remote.IDRemoteServices;
 
 // add needed import declarations
 
 public class RenewPasswordTest extends BubbleDocsServiceTest {
 
-	private static final String USER_TOKEN = "bla bla bla";
-	private static final String USER_TOKEN_NOT_LOGGED = "bla bla bla";
-
-	@Mocked
-	IDRemoteServices myService;
+	private static String USER_TOKEN;
+	private static final String USER_TOKEN_NOT_EXISTS="bla";
+	private static  String USER_TOKEN_NOT_LOGGED;
 	
-    @Test
-    public void success() {
-    	
-        RenewPasswordService service = new RenewPasswordService( USER_TOKEN);
-        service.execute();
-    }
-   
-    @Test
-    public void remoteFail(){        
-    	
-    	new Expectations(){{
-    		myService.renewPassword(USER_TOKEN); result = new RemoteInvocationException();
-    		
-    	}};
-    	
-    	RenewPasswordService service = new RenewPasswordService( USER_TOKEN);
-    	service.execute();
-    }
-    
-    
-    @Test(expected = UserNotInSessionException.class)
-    public void userNotLogged() {
-    	
-    	 removeUserFromSession(USER_TOKEN);
-    	 RenewPasswordService service = new RenewPasswordService( USER_TOKEN);
-         service.execute();
-    }    
- 
-    @Test(expected = UserNotInSessionException.class)
-    public void userTokenInvalid() {
-    	
-    	 removeUserFromSession(USER_TOKEN);
-    	 RenewPasswordService service = new RenewPasswordService( USER_TOKEN_NOT_LOGGED);
-         service.execute();
-    }   
-    
+	
+	 public void populate4Test() {
 
+	    unPopulate4Test();
+	   	
+	   	Bubbledocs bd = Bubbledocs.getInstance();
+	    	
+	    	
+	  	//Cria utilizador
+	   	Utilizador user = createUser("mas", "maria@email.com", "Maria Santos");
+	
+	   	USER_TOKEN = addUserToSession(user.getUsername());
+	   	
+	 }
+	 
+	@Mocked
+	IDRemoteServices remote;
+	
 //    @Test
-//    public <M extends Runnable & ResultSet> void someTest() {
-//        M mock = new MockUp<M>() {
-//           @Mock void run() { ...do something... }
-//           @Mock boolean next() { return true; }
-//        }.getMockInstance();
-//
-//        mock.run();
-//        assertTrue(mock.next());
+//    public void success() {
+//    	
+//        RenewPasswordService service = new RenewPasswordService( USER_TOKEN);
+//        service.execute();
 //    }
 //    
-//
-//    @Test(expected = UnavailableServiceException.class)
-//    public <RenewPasswordService extends Runnable> void userTokenInvalidTest() {
-//    	RenewPasswordService mock = new MockUp<RenewPasswordService>() {
-//           @Mock void execute() {  }
-//           @Mock String getResult() { ; }
-//        }.getMockInstance();
-//
-//        mock.run();
-//        assertTrue(mock.next());
-//    }
+    
+	@Test(expected = UserNotInSessionException.class)
+	public void remoteFails() {
+	    
+		new StrictExpectations(){
+	    	
+	    	{
+	    		remote = new IDRemoteServices();
+	    		remote.renewPassword(USER_TOKEN);
+	    		result = new RemoteInvocationException();
+	    	}
+	    };
+    }
+    
+    
+//    @Test(expected = UserNotInSessionException.class)
+//    public void userNotLogged() {
+//    	
+//    	new MockUp<IDRemoteServices>() {
+//    		@Mock
+//   			public void renewPassword(String username){
+//   				throw new LoginBubbleDocsException();
+//   			}
+//    	};
+//    	
+//   		new RenewPasswordService(USER_TOKEN).execute();
+//    	
+//    }    
+    
+ 
+//    @Test(expected = UserNotInSessionException.class)
+//    public void userTokenInvalid() {
+//    	
+//    	new MockUp<IDRemoteServices>() {
+//    		@Mock
+//   			public void renewPassword(String username){
+//   				throw new RemoteInvocationException();
+//   			}
+//    	};
+//    	
+//   		new RenewPasswordService(USER_TOKEN).execute();
+//    }     
     
 }
