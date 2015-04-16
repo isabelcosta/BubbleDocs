a188eb1b1f6afc15540d948f69142968c9d902d4
Pedro Dias <psdd91@gmail.com>
2015-04-16 11:02:01 +0100

    CreateUser.java - falta uma cena

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/services/CreateUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/services/CreateUser.java
@@ -3,10 +3,14 @@ package pt.tecnico.ulisboa.essd.bubbledocs.services;
 import pt.tecnico.ulisboa.essd.bubbledocs.domain.Bubbledocs;
 import pt.tecnico.ulisboa.essd.bubbledocs.domain.Utilizador;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.BubbleDocsException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.LoginBubbleDocsException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.RemoteInvocationException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.UnavailableServiceException;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.UserNotInSessionException;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.DuplicateUsernameException;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.EmptyUsernameException;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.UnauthorizedOperationException;
+import pt.tecnico.ulisboa.essd.bubbledocs.services.remote.IDRemoteServices;
 
 
 public class CreateUser extends BubbleDocsService {
@@ -30,8 +34,12 @@ public class CreateUser extends BubbleDocsService {
 	protected void dispatch() throws BubbleDocsException {
 		
 		Bubbledocs bd = Bubbledocs.getInstance();
+		IDRemoteServices remote = new IDRemoteServices();
 		
 		try {
+			//invoke some method on remote
+			remote.createUser(newUsername, email);;
+			
 			if(bd.validSession(userToken) && bd.isRoot(userToken)){
 				refreshToken(userToken);
 		    	
@@ -40,11 +48,25 @@ public class CreateUser extends BubbleDocsService {
 					bd.addUtilizadores(user);
 				}
 	    	}
-		} catch (DuplicateUsernameException | EmptyUsernameException | UnauthorizedOperationException | UserNotInSessionException e) {
+		} catch (DuplicateUsernameException | EmptyUsernameException |
+				UnauthorizedOperationException | UserNotInSessionException e) {
 			System.err.println("Couldn't create User: " + e);
-		}
 			
-		
+		} catch (RemoteInvocationException e) {
+			throw new UnavailableServiceException();
+				
+			
+		}catch (LoginBubbleDocsException e) {
+			// Se esta excepção for apanhada vai verificar se foi por nao ser root ou sessao nao valida
+			try {
+				bd.validSession(userToken);
+				bd.isRoot(userToken);
+				
+			} catch (UnauthorizedOperationException | UserNotInSessionException ex) {
+				System.err.println("Couldn't delete User: " + ex);
+				throw ex;
+			}
+		}	
 	}
 }
 
