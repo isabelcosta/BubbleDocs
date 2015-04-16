55b209180fc46114f5b3a41915d9061c4ed217b3
Ines Garcia <inescunhagarcia@hotmail.com>
2015-04-16 17:40:17 +0100

    Excepcoes finalizadas

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/CannotLoadDocumentException.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/CannotLoadDocumentException.java
@@ -4,5 +4,16 @@ public class CannotLoadDocumentException extends BubbleDocsException {
 
 	private static final long serialVersionUID = 1L;
 	
+	private String conflictingDocument;
+	
+	
+	public CannotLoadDocumentException(String conflictingDocument) {
+		this.conflictingDocument = conflictingDocument;
+	}
+	
+	
+	public String conflictingDocument() {
+		return this.conflictingDocument;
+	}
 
 }
--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/CannotStoreDocumentException.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/exception/CannotStoreDocumentException.java
@@ -3,5 +3,17 @@ package pt.tecnico.ulisboa.essd.bubbledocs.exception;
 public class CannotStoreDocumentException extends BubbleDocsException {
 
 	private static final long serialVersionUID = 1L;
+
+
+	private String conflictingDocument;
 	
+	
+	public CannotStoreDocumentException(String conflictingDocument) {
+		this.conflictingDocument = conflictingDocument;
+	}
+	
+	
+	public String conflictingDocument() {
+		return this.conflictingDocument;
+	}
 }
