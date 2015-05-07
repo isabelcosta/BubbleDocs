54262f1fd722a02b4d84afbb25ce2a0807902161
Pedro Dias <psdd91@gmail.com>
2015-05-07 11:41:57 +0100

    CreateUserService.java

--- bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/services/local/CreateUserService.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/services/local/CreateUserService.java
@@ -2,6 +2,10 @@ package pt.tecnico.ulisboa.essd.bubbledocs.services.local;
 
 import pt.tecnico.ulisboa.essd.bubbledocs.domain.Utilizador;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.BubbleDocsException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.DuplicateEmailException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.DuplicateUsernameException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.InvalidEmailException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.InvalidUsernameException;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.RemoteInvocationException;
 import pt.tecnico.ulisboa.essd.bubbledocs.exception.UnavailableServiceException;
 import pt.tecnico.ulisboa.essd.bubbledocs.services.remote.IDRemoteServices;
@@ -26,14 +30,29 @@ public class CreateUserService extends IsRootService {
 	@Override
 	protected void dispatch_root() throws BubbleDocsException {
 		
-		IDRemoteServices remote = new IDRemoteServices();
-		
-		try {
-			remote.createUser(newUsername, email);
-		}catch (RemoteInvocationException e) {
-			throw new UnavailableServiceException();	
+		if(this.newUsername == null || this.newUsername.equals("")){
+			throw new InvalidUsernameException() ;
 			
 		}	
+		if(this.email == null || this.email.equals("")){
+			throw new InvalidEmailException();
+		}
+		
+		for(Utilizador user : _bd.getUtilizadoresSet()){
+			
+			if(user.getEmail().equals(this.email)){
+				throw new DuplicateEmailException();
+				}
+			
+			
+			
+			if(user.getUsername().equals(this.newUsername)){
+				throw new DuplicateUsernameException(newUsername);
+				}
+			
+			
+		}
+		
 		Utilizador user = new Utilizador(name, newUsername, email);
 		_bd.addUtilizadores(user);
 	}
