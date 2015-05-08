743258f67d6484613d4b6c5747b3635fcc9572bb
Isabel Costa <isabelcmdcosta@gmail.com>
2015-05-08 02:28:39 +0100

    closes #107

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/ulisboa/essd/bubbledocs/services/integrator/AssignBinaryFunctionToCellIntegrator.java
@@ -0,0 +1,43 @@
+package pt.tecnico.ulisboa.essd.bubbledocs.services.integrator;
+
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.BubbleDocsException;
+import pt.tecnico.ulisboa.essd.bubbledocs.services.local.AssignBinaryFunctionToCellService;
+
+public class AssignBinaryFunctionToCellIntegrator extends BubbleDocsIntegrator{
+	
+	private String _userToken;
+	private AssignBinaryFunctionToCellService _local;
+	private String _binaryFunctionToAssign;
+	private String _cellToFill;
+	private int _folhaId;
+    
+	public AssignBinaryFunctionToCellIntegrator(String userToken, int docId, String cellId, String binaryFunction) {
+		this._userToken = userToken;
+    	this._binaryFunctionToAssign = binaryFunction;
+        this._cellToFill = cellId;
+        this._folhaId = docId;
+	}
+
+	@Override
+	protected void dispatch() throws BubbleDocsException {
+		
+		/*
+		 * 
+		 *  Instância local
+		 * 
+		 * 
+		 */
+
+//ter atencao as excepcoes ???
+		
+		_local = new AssignBinaryFunctionToCellService(_userToken, _folhaId, _cellToFill, _binaryFunctionToAssign);
+		_local.execute();
+		
+		/*
+		 * 
+		 * Nao existe metodo Remoto
+		 * 
+		 */
+		
+	}
+}
