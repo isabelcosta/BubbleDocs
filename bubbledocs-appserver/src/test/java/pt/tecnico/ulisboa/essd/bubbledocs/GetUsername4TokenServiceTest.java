2f3d1f190370827555b6ea0aea5263cee2658785
Pedro Dias <psdd91@gmail.com>
2015-05-10 19:31:43 +0100

    GetUsername4TokenServiceTest.java

---
+++ bubbledocs-appserver/src/test/java/pt/tecnico/ulisboa/essd/bubbledocs/GetUsername4TokenServiceTest.java
@@ -0,0 +1,62 @@
+package pt.tecnico.ulisboa.essd.bubbledocs;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import pt.tecnico.ulisboa.essd.bubbledocs.domain.Utilizador;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.InvalidTokenException;
+import pt.tecnico.ulisboa.essd.bubbledocs.exception.UserNotInSessionException;
+
+import pt.tecnico.ulisboa.essd.bubbledocs.services.local.GetUsername4TokenService;
+
+public class GetUsername4TokenServiceTest extends BubbleDocsServiceTest {
+
+	// the tokens
+	private String ars;
+
+	private static final String USERNAME = "ars";
+	private static final String TOKEN_INVALID = "no-one";
+	private static final String EMPTY_TOKEN = "";
+	private static final String EMAIL = "pedro@tecnico.ulisboa.pt";
+	private static final String NAME = "António Rito Silva";
+
+
+	private Utilizador user;
+
+	public void populate4Test() {
+
+		user = createUser(USERNAME, EMAIL, NAME);
+		ars = addUserToSession(USERNAME);
+	}
+
+	@Test
+	public void success() {
+
+		GetUsername4TokenService userToken = new GetUsername4TokenService(ars);
+		userToken.execute();
+
+		assertEquals(user.getUsername(), userToken.getUsername());
+
+	}
+
+	//Utilizador fora da sessão
+
+	@Test(expected = UserNotInSessionException.class)
+	public void userOutOfSession() {
+
+		GetUsername4TokenService userToken = new GetUsername4TokenService(TOKEN_INVALID);
+		userToken.execute();
+	}
+
+
+	//Token inválido
+
+	@Test(expected = InvalidTokenException.class)
+	public void tokenDoesNotExist() {
+
+		GetUsername4TokenService userToken = new GetUsername4TokenService(EMPTY_TOKEN);
+		userToken.execute();
+	}
+}
